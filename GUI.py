e14f19fca65139d502877b52b233160e50986391
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-08-21 18:34:32 -0300

    Foi adicionado os pocisionamentos na classe GUI

--- GUI.py
+++ GUI.py
@@ -34,7 +34,7 @@ class Gui():
         # listbox + scrollbar
         self.listClientes = Listbox(self.window, width=100)
         self.scrollClientes = Scrollbar(self.window)
-        
+
         # botões
         self.btnViewAll = Button(self.window, text="Ver Todos")
         self.btnBuscar = Button(self.window, text="Buscar")
@@ -42,3 +42,26 @@ class Gui():
         self.btnUpdate = Button(self.window, text="Atualizar Selecionados")
         self.btnDel = Button(self.window, text="Deletar Selecionados")
         self.btnClose = Button(self.window, text="Fechar")
+
+        self._setup_layout()
+
+    def _setup_layout(self):
+        self.lblnome.grid(row=0, column=0)
+        self.lblsobrenome.grid(row=1, column=0)
+        self.lblemail.grid(row=2, column=0)
+        self.lblcpf.grid(row=3, column=0)
+
+        self.entNome.grid(row=0, column=1, padx=50, pady=50)
+        self.entSobrenome.grid(row=1, column=1)
+        self.entEmail.grid(row=2, column=1)
+        self.entCPF.grid(row=3, column=1)
+
+        self.listClientes.grid(row=0, column=2, rowspan=10)
+        self.scrollClientes.grid(row=0, column=6, rowspan=10)
+
+        self.btnViewAll.grid(row=4, column=0, columnspan=2)
+        self.btnBuscar.grid(row=5, column=0, columnspan=2)
+        self.btnInserir.grid(row=6, column=0, columnspan=2)
+        self.btnUpdate.grid(row=7, column=0, columnspan=2)
+        self.btnDel.grid(row=8, column=0, columnspan=2)
+        self.btnClose.grid(row=9, column=0, columnspan=2)
