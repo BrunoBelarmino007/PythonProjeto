2fcef6cfa31519f3f07d261a5224195872d579de
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-08-21 18:49:14 -0300

    Foi adicionado a classe TransactionObject na classe Backend, juntamente com a conexão e desconexão com o banco clientes.db

---
+++ Backend.py
@@ -0,0 +1,16 @@
+import sqlite3 as sql
+
+class TransactionObject():
+    database   = "clientes.db"
+    conn       = None
+    cur        = None
+    connected  = False
+
+    def connect(self):
+        TransactionObject.conn = sql.connect(TransactionObject.database)
+        TransactionObject.cur  = TransactionObject.conn.cursor()
+        TransactionObject.connected = True
+
+    def disconnect(self):
+        TransactionObject.conn.close()
+        TransactionObject.connected = False
