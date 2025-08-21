4a7fedb7faf04d471079a430eeb2bb8d7ff2c2e7
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-08-21 18:25:44 -0300

    Foi adicionado as variáveis de entrada na classe GUI

--- GUI.py
+++ GUI.py
@@ -5,9 +5,16 @@ class Gui():
         Classe da Interface Gráfica
     """
     
-x_pad = 5
-y_pad = 3
-width_entry = 30
+    def __init__(self):
+        self.x_pad = 5
+        self.y_pad = 3
+        self.width_entry = 30
 
-window = Tk()
-window.wm_title( "PYSQL version 1.0" )
+        self.window = Tk()
+        self.window.wm_title("PYSQL version 1.0")
+
+        # variáveis de entrada
+        self.txtNome = StringVar()
+        self.txtSobrenome = StringVar()
+        self.txtEmail = StringVar()
+        self.txtCPF = StringVar()
