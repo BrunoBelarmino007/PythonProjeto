8fca451f2a6c7dbb78761a2eb2f6677258fd502b
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2025-08-21 19:31:51 -0300

    Foi adicionado as funções de visualização e busca de clientes

--- Aplication.py
+++ Aplication.py
@@ -4,3 +4,18 @@ from tkinter import END, messagebox
 
 app = None
 selected = None 
+
+def view_command():
+    rows = core.view()
+    app.listClientes.delete(0, END)
+    for r in rows:
+        app.listClientes.insert(END, r)
+
+def search_command():
+    app.listClientes.delete(0, END)
+    rows = core.search( app.txtNome.get(),
+                        app.txtSobrenome.get(),
+                        app.txtEmail.get(),
+                        app.txtCPF.get())
+    for r in rows:
+        app.listClientes.insert(END, r)
